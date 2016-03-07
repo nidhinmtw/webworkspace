ce411ae646ea5f91de505b560310d5bc8b7b6ae5
nidhinmtw <nidhinmtw@gmail.com>
2016-03-07 23:39:57 +0800

    babel config added

--- webpackreact/kanban_app/webpack.config.js
+++ webpackreact/kanban_app/webpack.config.js
@@ -14,6 +14,12 @@ const common = {
 	entry: {
 		app: PATHS.app
 	},
+	// Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  	resolve: {
+  		extensions: ['', '.js', '.jsx']
+  	},
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
@@ -23,6 +29,16 @@ const common = {
 			{
 				test: /\.css$/,
 				loaders: ['style', 'css'],
+				include: PATHS.app
+			},
+			{
+				test: /\.jsx?$/,
+				// Enable caching for improved performance during development
+		        // It uses default OS directory by default. If you need something
+		        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+				loaders: ['babel?cacheDirectory'],
+				// Parse only app files! Without this it will go through entire project.
+        		// In addition to being slow, that will most likely result in an error.
 				include: PATHS.app
 			}
 		]
