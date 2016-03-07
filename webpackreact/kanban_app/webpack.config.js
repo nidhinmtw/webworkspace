5a44ea6ab80761e595cbe41c3912398ac076d520
nidhinmtw <nidhinmtw@gmail.com>
2016-03-08 00:00:47 +0800

    react app starting

--- webpackreact/kanban_app/webpack.config.js
+++ webpackreact/kanban_app/webpack.config.js
@@ -36,7 +36,11 @@ const common = {
 				// Enable caching for improved performance during development
 		        // It uses default OS directory by default. If you need something
 		        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
-				loaders: ['babel?cacheDirectory'],
+				loader: 'babel',
+				query: {
+					cacheDirectory: true,
+					presets: ['react', 'es2015', 'survivejs-kanban']
+				},
 				// Parse only app files! Without this it will go through entire project.
         		// In addition to being slow, that will most likely result in an error.
 				include: PATHS.app
