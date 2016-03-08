7dfa78600b580f47fd1cb424280beff957ea5203
nidhinmtw <nidhinmtw@gmail.com>
2016-03-09 00:14:48 +0800

    File renamed to jsx and added first JSX to render

---
+++ webpackreact/kanban_app/app/index.jsx
@@ -0,0 +1,7 @@
+require('./main.css');
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDom.render(<App />, document.getElementById('app'));
