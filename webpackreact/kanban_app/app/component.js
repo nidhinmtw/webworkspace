c23d2f5498335033f3d241f374ce0017ce645ab4
nidhinmtw <nidhinmtw@gmail.com>
2016-03-13 13:15:37 +0800

    added Babel hmr for Hot module replacement with react elements

--- webpackreact/kanban_app/app/component.js
+++ webpackreact/kanban_app/app/component.js
@@ -1,3 +1,5 @@
+// THIS FILE, HAVING SERVED IT'S PURPOSE IN UNDERSTANDING REACT, NO LOGNER IS INVOLVED IN THE APP IN DEVELOPMENT.
+
 module.exports = function () {
 	var element = document.createElement('h1');
 	element.innerHTML = 'Hello World!!!!!!';
